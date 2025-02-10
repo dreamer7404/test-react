38947e9d661187f7afb49b0b0fb1d75dc464a558
안경수 <dreamer7404@daum.net>
2025-02-10 23:44:53 +0900

    vvvv

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.- ahnks01 - ahnks01
+          Edit <code>src/App.js</code> and save to reload.- ahnks01 - ahnks01 - 333
         </p>
         <a
           className="App-link"
